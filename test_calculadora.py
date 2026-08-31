fb7365dd3954245e9ee3ffc5ac354203fb0e4937
guilhermeyuk-yukio <guilhermeyuk@gmail.com>
2026-08-31 20:52:20 -0300

    Adiciona mais funções e testes na calculadora

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,4 +1,18 @@
-from calculadora import soma
+from calculadora import soma, subtracao, multiplicacao, divisao
+import pytest
 
 def test_soma():
     assert soma(2, 3) == 5
+
+def test_subtracao():
+    assert subtracao(10, 4) == 6
+
+def test_multiplicacao():
+    assert multiplicacao(3, 4) == 12
+
+def test_divisao():
+    assert divisao(10, 2) == 5
+
+def test_divisao_por_zero():
+    with pytest.raises(ValueError):
+        divisao(10, 0)
